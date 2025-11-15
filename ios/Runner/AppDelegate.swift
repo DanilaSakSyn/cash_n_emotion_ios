2f229fa7988e5731156b816722dcbf517938aa9f
DanilaSakSyn <danilasak@syndicate-g.com>
2025-11-15 22:17:25 +0300

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,7 @@
 import Flutter
 import UIKit
 import flutter_local_notifications
-
+import AppTrackingTransparency
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
